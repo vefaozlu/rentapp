41ac62f38fa8d1ec90cb8501a9d51b50f3b68ebe
Vefa Ozlu <vefaozlu01@gmail.com>
2022-11-18 10:17:36 +0300

    dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
