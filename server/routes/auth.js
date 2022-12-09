8d00ed0fab843a6ca00051ce167fea7fa29d9a84
Vefa Ozlu <vefaozlu01@gmail.com>
2022-12-09 20:02:50 +0300

    JWT verification

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 let Auth = require('../models/auth.model');
 
@@ -9,6 +12,8 @@ router.route('/register').post(async (req, res) => {
     const phoneNumber = req.body.phone_number;
     const renterId = req.body.renter_id;
     const balanceId = req.body.balance_id;
+
+    const key = process.env.TOKEN_SECRET;
 
     if (!(username && email && password)) {
         return res.status(400).json('All Fields Must Be Filled.');
@@ -28,8 +33,15 @@ router.route('/register').post(async (req, res) => {
         balanceId,
     });
 
+    const data = {
+        'email': email,
+        'username': username,
+    }
+
+    const token = jwt.sign({ data: data, exp: Math.floor(Date.now() / 1000) + 86400 }, key);
+
     auth.save()
-        .then(() => res.status(200).json('User registered'))
+        .then(() => res.status(200).json(token))
         .catch(err => res.status(400).json('Error' + err));
 });
 
@@ -38,11 +50,15 @@ router.route('/login').post(async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    if(!(username || email)) {
+    const key = process.env.TOKEN_SECRET;
+
+    if (!(username || email)) {
         return res.status(401).json('Enter a username or email');
     }
 
-    const user = email != null ? await Auth.findOne({ email: email}) : await Auth.findOne({ username: username});
+    const user = email != null
+        ? await Auth.findOne({ email: email })
+        : await Auth.findOne({ username: username });
 
     if (!user) {
         return res.status(404).json('User not found');
@@ -52,7 +68,33 @@ router.route('/login').post(async (req, res) => {
         return res.status(401).json('Uncorrect credentials');
     }
 
-    return res.status(200).json(user);
+    const data = {
+        'email': email,
+        'username': username,
+    }
+
+    const token = jwt.sign({ data: data, exp: Math.floor(Date.now() / 1000) + 86400 }, key);
+
+    return res.status(200).json({"user": user, "token": token});
+});
+
+router.route('/verify').post((req, res) => {
+    const key = process.env.TOKEN_SECRET;
+    const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+
+    try {
+        const token = req.header(tokenHeaderKey);
+
+        const verified = jwt.verify(token, key);
+
+        if (verified) {
+            return res.status(200).json('Verified');
+        } else {
+            return res.status(401).json('Token expired')
+        }
+    } catch (err) {
+        return res.status(401).json(err);
+    }
 });
 
 module.exports = router;
