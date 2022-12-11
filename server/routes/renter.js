3c52f217edcbbd070ee9e04630fab563213f62b7
Vefa Ozlu <vefaozlu01@gmail.com>
2022-12-11 16:28:59 +0300

    Renter route updates

--- server/routes/renter.js
+++ server/routes/renter.js
@@ -16,7 +16,7 @@ router.route('/').get(async (req, res) => {
         );
         res.status(200).json(results.rows);
     } catch (err) {
-        res.status(400).json('Error' + err);
+        res.status(400).send('Error' + err);
     }
 });
 
@@ -28,13 +28,50 @@ router.route('/:id').get(async (req, res) => {
             [id]
         );
 
-        console.log(results.rows[0].toMap((element) => element.renter_name));
-
         res.status(200).json(results.rows);
     } catch (err) {
-        res.status(400).json('Error' + err)
+        res.status(400).send('Error' + err)
     }
 });
 
+router.route('post').post(async (req, res) => {
+    try {
+        const { name, email, bank_info } = req.params;
+        const result = await pool.query(
+            "INSERT INTO renter (name, email, bank_info) VALUES ($1, $2, $3) RETURNING *",
+            [name, email, bank_info]
+        );
+        res.status(200).send('User created');
+    } catch (err) {
+        res.status(400).send('User create failed ' + err);
+    }
+});
+
+router.route('/update/:id').put(async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, bank_info } = req.body;
+        const result = await pool.query(
+            "UPDATE renter SET name = $1, email = $2, bank_info = $3 WHERE id = $4 RETURNING *",
+            [name, email, bank_info, id]
+        );
+        res.status(200).json(result.rows);
+    } catch (err) {
+        res.status(400).send('Update failed ' + err);
+    }
+});
+
+router.route('/delete/:id').delete(async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query(
+            "DELETE FROM renter WHERE id = $1",
+            [id]
+        );
+        res.status(200).send('User deleted');
+    } catch (err) {
+        res.status(400).send('Delete failed ' + err);
+    }
+});
 
 module.exports = router;
